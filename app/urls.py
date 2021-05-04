e97614af7d9d87046a08815c65cd589b3e4a4c19
IngabireTina <ingabire067@gmail.com>
2021-05-04 15:18:18 -0400

    added urls in new app

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,13 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns=[
-    # path('', dashboard, name='dashboard'),
+    path('', dashboard, name='dashboard'),
+    path('signup/', signup, name='signup'),
+    path('profile/<username>/', profile, name='profile'),
+    path('join_hood/<id>', join_hood, name='join-hood'),
+    path('leave_hood/<id>', leave_hood, name='leave-hood'),
+    path('hood/<id>', hood, name='hood'),
+    path('<id>/create_post', post, name='post'),
+    path('<id>/create_business', business, name='business'),
+    path('search/', search_hood, name='search'),
 ]
